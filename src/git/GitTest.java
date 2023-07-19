fe7105bc88a5ad1b8686ffb9c061307afc1b0476
LWH <1191782051@qq.com>
2023-07-19 20:31:09 +0800

    third commit

--- src/git/GitTest.java
+++ src/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello git2");
+        System.out.println("Hello git3");
     }
 
 }
